d196701ae7bd891cc4619b11c75c70b1df18d5cb
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-01-26 22:58:40 +0100

    ignore: sketch out next section.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
+  state = {
+    count: 1,
+    imgUrl: 'https://picsum.photos/200'
+  };
+
+  styles = {
+    fontSize: 12,
+    fontWeight: 'bold'
+  };
+
   render() {
     return (
-      <div>
-        <h3>This is the Counter</h3>
-      </div>
+      <React.Fragment>
+        <div>
+          <img src={this.state.imgUrl} alt="" />
+        </div>
+        <span style={{ fontSize: 15, fontWeight: 'bold' }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+
+    return classes;
   }
 }
 
