74863fbcf4385f731a6525fdc074f3552c01c91d
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-02-04 16:50:23 +0100

    Add global error handling with axios interceptors.

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Table from './common/table';
+
+const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+
+axios.interceptors.responses.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log('Logging the error.', error);
+    alert('an unexpected error occurred.');
+  }
+
+  return Promise.reject(error);
+});
 
 class Posts extends Component {
 
@@ -15,16 +32,58 @@ class Posts extends Component {
     { key: 'delete', content: (item) => <button className="btn btn-danger btn-sm" onClick={() => this.handleDelete(item)}>Delete</button> },
   ];
 
-  handleAdd = () => {
-    console.log('adding post..');
+
+  async componentDidMount() {
+    const { data: posts } = await axios.get(apiEndpoint);
+    this.setState({ posts });
+  }
+
+  handleCreate = async () => {
+    const obj = { title: 'a', body: 'b' };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+
+    this.setState(prevState => {
+      return {
+        posts: [
+          post,
+          ...prevState.posts
+        ]
+      };
+    });
   };
 
-  handleUpdate = post => {
-    console.log('updating post..', post);
+  handleUpdate = async post => {
+    post.title = 'UPDATED';
+    await axios.put(`${apiEndpoint}/${post.id}`, post);
+    // axios.patch(`${apiEndpoint}/${post.id}`, { title: post.title });
+
+    this.setState(prevState => {
+      const posts = [...prevState.posts];
+      const index = posts.indexOf(post);
+      posts[index] = {...post};
+
+      return { posts };
+    });
   };
 
-  handleDelete = post => {
-    console.log('deleting post..', post);
+  handleDelete = async post => {
+    const originalPosts = this.state.posts;
+
+    this.setState(prevState => {
+      const posts = this.state.posts.filter(p => p.id !== post.id);
+
+      return { posts };
+    });
+
+    try {
+      await axios.delete(`${apiEndpoint}/${post.id}`);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        alert('This post does not exist or has already been deleted.');
+      }
+
+      this.setState({ posts: originalPosts });
+    }
   };
 
   handleSort = (sortColumn) => {
@@ -36,7 +95,7 @@ class Posts extends Component {
   render() {
     return (
       <div>
-        <button className="btn btn-primary mb-3" onClick={this.handleAdd}>
+        <button className="btn btn-primary mb-3" onClick={this.handleCreate}>
           Create New Thread
         </button>
         <Table
