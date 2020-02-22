00472ee225d9d5933ed3d44d3d72ff2981bed36b
Luca J <34938395+luke-jj@users.noreply.github.com>
2020-02-22 20:52:54 +0100

    Make table responsive for smaller layout.

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -5,17 +5,19 @@ import TableBody from './tablebody';
 
 const Table = ({ sortColumn, columns, data, onSort }) => {
   return (
-    <table className="table">
-      <TableHeader
-        sortColumn={sortColumn}
-        columns={columns}
-        onSort={onSort}
-      />
-      <TableBody
-        data={data}
-        columns={columns}
-      />
-    </table>
+    <div className="table-responsive-xl">
+      <table className="table">
+        <TableHeader
+          sortColumn={sortColumn}
+          columns={columns}
+          onSort={onSort}
+        />
+        <TableBody
+          data={data}
+          columns={columns}
+        />
+      </table>
+    </div>
   );
 }
 
